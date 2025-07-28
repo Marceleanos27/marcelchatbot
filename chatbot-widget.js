5ad07ffabf8a521238b6989e5735ed42577a048d
Marceleanos27 <lehocky.business@gmail.com>
2025-07-28 15:02:00 +0200

    Update chatbot-widget.js

--- chatbot-widget.js
+++ chatbot-widget.js
@@ -7,8 +7,8 @@
   iframe.style.position = "fixed";
   iframe.style.bottom = "20px";
   iframe.style.right = "20px";
-  iframe.style.width = "360px";
-  iframe.style.height = "500px";
+  iframe.style.width = "400px";
+  iframe.style.height = "550px";
   iframe.style.border = "none";
   iframe.style.borderRadius = "18px";
   iframe.style.zIndex = "99999";
