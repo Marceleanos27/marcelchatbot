4f122257a7e87b4fbb789e9809561256ad2edd99
Marceleanos27 <lehocky.business@gmail.com>
2025-07-29 20:41:30 +0200

    Update book.j

--- api/book.js
+++ api/book.js
@@ -1,8 +1,3 @@
-let bookingMode = false;
-let bookingStep = 0;
-let bookingData = {};
-
-
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
